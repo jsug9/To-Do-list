8462b1858fbf29179621f6229bc86f969462ac39
Augusto <jesus_augga_99@icloud.com>
2022-03-22 14:33:47 -0500

    add h1

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'you'], ' ');
   element.classList.add('hello');
 
   return element;
