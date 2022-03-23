53e11b39bd5c0bd173124e256b526ad2483719bb
Augusto <jesus_augga_99@icloud.com>
2022-03-23 09:19:10 -0500

    add functionality to add, remove and mark todos

--- src/index.js
+++ src/index.js
@@ -1,33 +1,75 @@
 import './style.css';
-import todos from './modules/todos.js';
-import todoList from './modules/htmlElements.js';
+import Todos from './modules/todos.js';
 import storageAvailable from './modules/storageAvailable.js';
-
-const sortedTodos = todos.sort(function(a, b) {
-  if ( a.index < b.index ){
-    return -1;
-  }
-  if ( a.index > b.index ){
-    return 1;
-  }
-  return 0;
-});
-
-sortedTodos.forEach((todo) => {
-  const content = document.createElement('li');
-  content.setAttribute('class', 'todo');
-  content.innerHTML = `
-    <div class="todo-left">
-      <input type="checkbox" class="checkbox" ${todo.isComplete ? 'checked' : ''}>
-      <label class="todo-text" contenteditable="true">${todo.description}</label>
-    </div>
-    <button class="remove-button"><span class="iconify delete" data-icon="fa-solid:trash-alt"></span></span></button>
-  `;
-  todoList.appendChild(content);
-});
 
 const todoInput = document.getElementById('todo-input')
 
-const checkboxes = document.getElementsByClassName('checkbox');
 const todoTexts = document.getElementsByClassName('todo-text');
-const removeButtons = document.getElementsByClassName('remove-button');
+
+// const sortedTodos = todos.sort(function(a, b) {
+//   if ( a.index < b.index ){
+//     return -1;
+//   }
+//   if ( a.index > b.index ){
+//     return 1;
+//   }
+//   return 0;
+// });
+
+const todos = new Todos();
+
+if (storageAvailable('localStorage')) {
+  todos.add({
+    description: 'Take a bath',
+    isComplete: false,
+    index: 0,
+  });
+  todos.add({
+    description: 'Brush my teeth',
+    isComplete: true,
+    index: 1,
+  });
+  todos.add({
+    description: 'Finish the assignment',
+    isComplete: false,
+    index: 2,
+  });
+
+  todos.checkStorage();
+
+  todos.showTodos();
+
+  for (let i = 0; i < todos.todos.length; i += 1) {
+    const removeButtons = document.querySelectorAll('.remove-button');
+    removeButtons[i].addEventListener('click', () => {
+      todos.delete(i);
+    });
+  }
+
+  for (let i = 0; i < todos.todos.length; i += 1) {
+    const checkboxes = document.querySelectorAll('.checkbox');
+    checkboxes[i].addEventListener('change', (e) => {
+      if(e.target.checked){
+        todos.complete(i)
+      } else {
+        todos.notComplete(i)
+      }
+      todos.save();
+      console.log(todos.todos);
+    });
+  }
+
+  todoInput.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+      todos.add({
+        description: todoInput.value,
+        isComplete: false,
+        index: todos.todos.length,
+      });
+      todos.save();
+      console.log(todos.todos);
+      todoInput.value = '';
+      window.location.reload();
+    }
+  });
+};
--- src/modules/todos.js
+++ src/modules/todos.js
@@ -1,19 +1,62 @@
-const todos = [
-  {
-    description: 'Take a bath',
-    isComplete: false,
-    index: 1,
-  },
-  {
-    description: 'Brush my teeth',
-    isComplete: true,
-    index: 0,
-  },
-  {
-    description: 'Finish the assignment',
-    isComplete: false,
-    index: 2,
-  },
-];
+const newTodos = [];
 
-export default todos;
+const todoList = document.getElementById('todo-list');
+export default class Todos {
+  constructor() {
+    this.todos = [];
+  }
+
+  save = () => {
+    window.localStorage.setItem('todos', JSON.stringify(this.todos));
+    for (let i = 0; i < this.todos.length; i += 1) {
+      window.localStorage.setItem(i.toString(), JSON.stringify(this.todos[i]));
+    }
+  }
+
+  add = (todo) => {
+    this.todos.push(todo);
+  }
+
+  delete = (index) => {
+    this.todos.splice(index, 1);
+    this.save();
+    window.location.reload();
+  }
+
+  complete = (index) => {
+    this.todos[index].isComplete = true;
+  }
+
+  notComplete = (index) => {
+    this.todos[index].isComplete = false;
+  }
+
+  edit = (index, description) => {
+    this.todos[index].description = description;
+  }
+
+  checkStorage = () => {
+    if (window.localStorage.getItem('todos') !== null) {
+      const array = JSON.parse(window.localStorage.getItem('todos'));
+      for (let i = 0; i < array.length; i += 1) {
+        newTodos[i] = JSON.parse(window.localStorage.getItem(i.toString()));
+      }
+      this.todos = newTodos;
+    }
+  }
+
+  showTodos = () => {
+    for (let i = 0; i < this.todos.length; i += 1) {
+      const content = document.createElement('li');
+      content.setAttribute('class', 'todo');
+      content.innerHTML = `
+        <div class="todo-left">
+          <input type="checkbox" class="checkbox" ${this.todos[i].isComplete ? 'checked' : ''}>
+          <label class="todo-text" contenteditable="true">${this.todos[i].description}</label>
+        </div>
+        <button class="remove-button"><span class="iconify delete" data-icon="fa-solid:trash-alt"></span></span></button>
+      `;
+      todoList.appendChild(content);
+    }
+  }
+}
